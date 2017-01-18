99b4abf8a10ca33b8cd9523b386de736ff5c2ba1
Jeroen de Schepper <Jeroen de Schepper>
2017-01-18 10:29:22 +0100

    added opaque buttons

--- PythonApplication6/PythonApplication6/Project intro.py
+++ PythonApplication6/PythonApplication6/Project intro.py
@@ -4,6 +4,11 @@
 import pygame
 import sys
 
+green = (0, 255, 0)
+red = (255, 0, 0)
+blue = (0, 64, 128)
+
+
 # Handle pygame events
 def process_events():
     for event in pygame.event.get():
@@ -21,10 +26,27 @@ def program():
     
     # Set the resolution
     Background = pygame.image.load("Background.jpg")
-    Background = pygame.transform.scale(Background, (1280, 720))
+    Background = pygame.transform.scale(Background, (size))
     screen = pygame.display.set_mode(size)
     screen.blit(Background,(0, 0))
+    
+    x = width/10
+    y = height/1.25
 
+    start_button = pygame.Surface((170, 50))
+    start_button.set_alpha(150)
+    screen.blit(start_button,(x, y))
+    
+    y = height/1.4
+    tutorial_button = pygame.Surface((170, 50))
+    tutorial_button.set_alpha(150)
+    screen.blit(tutorial_button,(x, y))
+
+    y = height/1.6
+    exit_button = pygame.Surface((170, 50))
+    exit_button.set_alpha(150)
+    screen.blit(exit_button,(x, y))
+    
     while not process_events():   
         
         # Flip the screen
@@ -33,3 +55,4 @@ def program():
 
 # Start the program
 program()
+
