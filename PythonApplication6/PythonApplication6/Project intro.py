ba0cc54aff79d34e01a15fc184213cedff0231c0
Raoul van Duivenvoorde <raoul@vanduivenvoorde.com>
2017-01-18 12:08:44 +0100

    I trie to make the buttons interactive

--- PythonApplication6/PythonApplication6/Project intro.py
+++ PythonApplication6/PythonApplication6/Project intro.py
@@ -14,7 +14,16 @@ def process_events():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
            sys.exit()
-
+def mouse(x,y,w,h, button, screen):
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+    if x + w > mouse[0] > x and y + h > mouse[0] > y:
+        button.set_alpha(550)
+        screen.blit(button,(x, y))
+    else: 
+        button.set_alpha(150)   
+        screen.blit(button,(x, y))
+    
 # Main program logic
 def program():
     width = 1280
@@ -29,13 +38,17 @@ def program():
     Background = pygame.transform.scale(Background, (size))
     screen = pygame.display.set_mode(size)
     screen.blit(Background,(0, 0))
-    
+ 
+    xmouse, ymouse = pygame.mouse.get_pos()
     x = width/10
     y = height/1.25
-
-    start_button = pygame.Surface((170, 50))
-    start_button.set_alpha(150)
-    screen.blit(start_button,(x, y))
+    exit_button = pygame.Surface((170, 50))
+    if x + 170 > xmouse > x and y + 50 > ymouse > y:
+        exit_button.set_alpha(500)
+        screen.blit(exit_button,(x, y))
+    else: 
+        exit_button.set_alpha(150)   
+        screen.blit(exit_button,(x, y))
     
     y = height/1.4
     tutorial_button = pygame.Surface((170, 50))
@@ -43,10 +56,15 @@ def program():
     screen.blit(tutorial_button,(x, y))
 
     y = height/1.6
-    exit_button = pygame.Surface((170, 50))
-    exit_button.set_alpha(150)
-    screen.blit(exit_button,(x, y))
-    
+    highscore_button = pygame.Surface((170, 50))
+    highscore_button.set_alpha(150)
+    screen.blit(highscore_button,(x, y))
+
+    y = height/1.86
+    start_button = pygame.Surface((170, 50))
+    start_button.set_alpha(150)
+    screen.blit(start_button,(x, y))
+
     while not process_events():   
         
         # Flip the screen
