9873092d81f33a6c91715393ff57ea13e25f5c45
Jeroen de Schepper <Jeroen de Schepper>
2017-01-19 14:39:46 +0100

    .

--- PythonApplication6/PythonApplication6/Project intro.py
+++ PythonApplication6/PythonApplication6/Project intro.py
@@ -6,22 +6,17 @@ from pygame.locals import*
 import sys
 import math
 
-
-green = (0, 255, 0)
-red = (255, 0, 0)
-blue = (0, 64, 128)
-
 class Application:
     def __init__(self):
-        self.width = 1920
-        self.height = 1080
+        self.width = 1280
+        self.height = 720
         self.size = (self.width, self.height)
     
         # Start PyGame
         pygame.init()
     
         # Set the resolution
-        self.screen = pygame.display.set_mode((self.size), pygame.FULLSCREEN)
+        self.screen = pygame.display.set_mode((self.size))#, pygame.FULLSCREEN)
     
         self.phase = "intro"
 
@@ -30,12 +25,20 @@ class Application:
         self.highscore = Highscore(self, self.width, self.height)
         self.tutorial = Tutorial(self, self.width, self.height)
 
+    def back(self):
+        for event in pygame.event.get():
+            if event.type == KEYDOWN:
+                if event.key == K_BACKSPACE:
+                    self.application.phase = "intro"
+
     def application_loop(self):
         while not process_events():
             if self.phase == "intro":
                 self.intro.draw(self.screen)
             elif self.phase == "game":
                 self.game.draw(self.screen)
+            elif self.phase == "pause":
+                self.pause.draw(self.screen)
             elif self.phase == 'Highscore':
                 self.highscore.draw(self.screen) 
             elif self.phase == 'Tutorial':
@@ -58,7 +61,7 @@ class Intro:
     def draw (self, screen):
         screen.blit(self.Background,(0, 0))
         title_text = self.font.render("BattlePort", 1, (255,120,0))
-        screen.blit(title_text,((self.width / 15) , (self.height / 9) ))
+        screen.blit(title_text,((self.width / 15) , (self.height / 9)))
         self.exit_button.draw(screen)
         self.tutorial_button.draw(screen)
         self.highscore_button.draw(screen)
@@ -74,19 +77,19 @@ class Button:
         self.h = h
         self.surface = pygame.Surface((w, h))
         self.font = pygame.font.Font(None, 45)
-        self.width = 1280
-        self.heigth = 720
     def draw (self, screen):
         mouse_pos = pygame.mouse.get_pos()
         mouse_click = pygame.mouse.get_pressed()
         if self.x + 170 > mouse_pos[0] > self.x and self.y + 50 > mouse_pos[1] > self.y:
             screen.blit(self.surface, (self.x, self.y))
             button_text = self.font.render(self.text, 1, (255,255,255))
-            screen.blit(button_text,(( self.x + 5), (self.y + 11) ))
+            screen.blit(button_text,(( self.x + 5), (self.y + 11)))
             if mouse_click[0]:
                 print (self.text)
                 if self.text == 'Start':
                     self.application.phase = "game"
+                elif self.text == 'Pause':
+                    self.application.phase = "pause"
                 elif self.text == 'Tutorial':
                     self.application.phase = 'Tutorial'
                 elif self.text == 'Highscore':
@@ -96,19 +99,62 @@ class Button:
         else:
             screen.blit(self.surface, (self.x, self.y))
             button_text = self.font.render(self.text, 1, (255,120,0))
-            screen.blit(button_text,(( self.x + 5), (self.y + 11) ))
+            screen.blit(button_text,((self.x + 5), (self.y + 11)))
 
 class Game:
     def __init__ (self, application, width, height):
+        self.pause = Pause
+        self.surface = pygame.Surface((width, height))
         self.application = application
         self.Background = pygame.image.load("Speelbord.png")
         self.Background = pygame.transform.scale(self.Background, (width, height))
         self.font = pygame.font.SysFont('Arial', 150)
         self.width = width
         self.height = height
-    def draw (self, screen):
-        screen.blit(self.Background,(0,0))
+        
+        self.Ship1Move = Button(self.application, '', (width/70), (height/22.80), 55, 55)
+        self.Ship1Def  = Button(self.application, '', (width/70), (height/7.300), 55, 55)
+        self.Ship1Att  = Button(self.application, '', (width/70), (height/4.300), 55, 55)
 
+        self.Ship2Move = Button(self.application, '', (width/70), (height/2.580), 55, 55)
+        self.Ship2Def  = Button(self.application, '', (width/75), (height/2.075), 55, 55)
+        self.Ship2Att  = Button(self.application, '', (width/75), (height/1.739), 55, 55)
+
+        self.Ship3Move = Button(self.application, '', (width/75), (height/1.395), 55, 55)
+        self.Ship3Def  = Button(self.application, '', (width/75), (height/1.233), 55, 55)
+        self.Ship3Att  = Button(self.application, '', (width/75), (height/1.105), 55, 55)
+
+        self.Yes = Button(self.application, 'Yes', (self.width/50), (self.height/22.80), 55, 55)
+        self.No  = Button(self.application, 'No', (self.width/50), (self.height/7.300), 55, 55)
+
+    def draw (self, screen):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_BACKSPACE]:
+            self.pause.draw(self.screen)
+            
+        for event in pygame.event.get():
+            if event.type == KEYDOWN:
+                if event.key == K_BACKSPACE:
+                    screen.blit(self.surface,(0,0))
+                    screen.blit(self.surface,(0,0))                    
+                    Pause.draw(self, screen)
+        
+
+class Pause:
+    def __init__ (self, application, width, height):
+        self.application = application
+        self.width = width
+        self.height = height
+        self.Background = pygame.image.load("Speelbord.png")
+        self.Background = pygame.transform.scale(self.Background, (width, height))
+        
+
+    def draw(self, screen):
+        self.Yes.draw(screen)
+        self.No.draw(screen)
+        screen.blit(self.surface,(self.width, self.height))
+    
+        
 class Highscore:
     def __init__ (self, application, width, height):
         self.application = application
@@ -120,8 +166,9 @@ class Highscore:
     def draw (self, screen):
         screen.blit(self.Background,(0,0))
         title_text = self.font.render("Highscore", 1, (255,120,0))
-        screen.blit(title_text,((self.width / 15) , (self.height / 9) ))
-
+        screen.blit(title_text,((self.width / 15) , (self.height / 9)))
+        Application.back(self)
+    
 class Tutorial:
     def __init__ (self, application, width, height):
         self.application = application
@@ -133,48 +180,23 @@ class Tutorial:
     def draw (self, screen):
         screen.blit(self.Background,(0,0))
         title_text = self.font.render("Tutorial", 1, (255,120,0))
-        screen.blit(title_text,((self.width / 15) , (self.height / 9) ))
-              
+        screen.blit(title_text,((self.width / 15) , (self.height / 9)))
+        Application.back(self)
+          
                 
 # Handle pygame events
 def process_events():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-           sys.exit()
+            sys.exit()
         elif event.type == KEYDOWN:
-               if event.key == K_ESCAPE:
-                   sys.exit()
-                   return
-    
+            if event.key == K_ESCAPE:
+                sys.exit()
+
 # Main program logic
 def program():
     application = Application()
     application.application_loop()
-
-
-"""def toggle_fullscreen():
-    screen = pygame.display.get_surface()
-    tmp = screen.convert()
-    caption = pygame.display.get_caption()
-    cursor = pygame.mouse.get_cursor()  # Duoas 16-04-2007 
-    
-    w,h = screen.get_width(),screen.get_height()
-    flags = screen.get_flags()
-    bits = screen.get_bitsize()
-    
-    pygame.display.quit()
-    pygame.display.init()
-    
-    screen = pygame.display.set_mode((w,h),flags^FULLSCREEN,bits)
-    screen.blit(tmp,(0,0))
-    pygame.display.set_caption(*caption)
- 
-    pygame.key.set_mods(0) #HACK: work-a-round for a SDL bug??
- 
-    pygame.mouse.set_cursor( *cursor )  # Duoas 16-04-2007
-    
-    return screen"""
-
 
 # Start the program
 program()
